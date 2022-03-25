7b0b42cf31a06f154f63358fd51accd16245c3f5
pinargoktas <70344842+pinargoktas@users.noreply.github.com>
2022-03-25 18:13:34 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,5 +1,11 @@
 import logging
 import os
+from zipfile38 import ZipFile
+from zipfile38 import is_zipfile
+import zipfile38
+from google.cloud import storage
+import wget
+import pandas as pd
 
 from flask import Flask
 from secure import require_apikey
@@ -7,17 +13,53 @@ from secure import require_apikey
 
 app = Flask(__name__)
 
-
 @app.route('/health')
 def health():
     return 'It is alive!\n'
 
+@app.route('/data_puller')
+def data_puller():
+    url = os.environ[${URL}]
+    bucket_name = os.environ[${BUCKET}] #without gs://
+    file_name = os.environ[${FILE_NAME}]
+   	client = storage.Client($PROJECT_ID)
+    cf_path = '/tmp/{}'.format(file_name)
 
-@app.route('/hello')
-@require_apikey
-def hello():
-    return {'hello': 'world'}
+	bucket = client.get_bucket(bucket_name)
+	wget.download(url, cf_path)
+	blob = storage.Blob(file_name, bucket)
+ 	blob.upload_from_filename(cf_path)
 
+	print("""This Function was triggered by messageId published at""")
+    
+@app.route('/unzipper')
+def unzipper():
+    storage_client = storage.Client($PROJECT_ID)
+    bucket = storage_client.get_bucket(${BUCKET})
 
+    destination_blob_pathname = ${FILE_NAME}
+        
+    blob = bucket.blob(destination_blob_pathname)
+    zipbytes = io.BytesIO(blob.download_as_string())
+    
+    if is_zipfile(zipbytes):
+        with ZipFile(zipbytes, 'r') as myzip:
+            for contentfilename in myzip.namelist():
+                contentfile = myzip.read(contentfilename)
+                blob = bucket.blob(zipfilename_with_path + "/" + contentfilename)
+                blob.upload_from_string(contentfile)
+             
+@app.route('/txt2csv')
+def txt2csv():
+    fileName = ${FILE_NAME}
+    if ${FILE_NAME}[:-3] == '.txt'
+        df = pd.read_csv(fileName, sep=",")
+
+        storage_client = storage.Client($PROJECT_ID)
+        bucket = client.get_bucket(${BUCKET})
+        blob = bucket.blob(${FILE_NAME})
+ 	    blob.upload_from_string(df.to_csv(), 'text/csv')
+
+    
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
