b8f7b6e38a6fccaae957032b7033079d6c2720fb
pinargoktas <70344842+pinargoktas@users.noreply.github.com>
2022-03-29 17:47:01 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -18,4 +18,4 @@ def health():
     return 'It is alive!\n'
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get($PORT, 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get($PORT)))
