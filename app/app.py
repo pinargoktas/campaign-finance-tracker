f92514c31fc1bc9fc974f34824943c2fc0f1a412
pinargoktas <70344842+pinargoktas@users.noreply.github.com>
2022-04-05 13:42:29 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -13,9 +13,10 @@ from secure import require_apikey
 
 app = Flask(__name__)
 
+
 @app.route('/health')
 def health():
     return 'It is alive!\n'
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get($PORT)))
+#if __name__ == "__main__":
+app.run(debug=True, host='0.0.0.0', port=int(os.environ.get($PORT)))
